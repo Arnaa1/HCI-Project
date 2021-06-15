8bab1dc8692ce42b4cf614cad137be0227b50124
Ajla Arnautovic <ajla.arnautovic@socialexplorer.com>
2021-06-15 02:30:54 +0200

    Final editing

--- welcome.php
+++ welcome.php
@@ -27,97 +27,141 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
           background-size: cover;
         }
 
-html {
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-
-.column {
-  float: left;
-  width: 33.3%;
-  margin-bottom: 16px;
-  padding: 0 8px;
-}
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  margin: 8px;
-}
-
-.about-section {
-  padding: 50px;
-  text-align: center;
-  background-color: #360e94;
-  color: white;
-}
-
-.container {
-  padding: 0 16px;
-}
-
-
-
-.title {
-  color: grey;
-}
-
-.button {
-
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-}
-
-.button:hover {
-  background-color: #555;
-}
-
-@media screen and (max-width: 650px) {
-  .column {
-    width: 100%;
-    display: block;
-  }
-}
-      .button1 {
-      background-color: rgb(54, 14, 148);
-      border: none;
-      color: white;
-      padding: 15px 32px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      margin: 4px 2px;
-      cursor: pointer;
-      position: absolute;
-      top: 0;
-      right: 0;
-    }
-
-    .button {
-              background-color: #360e94;
-              border: none;
-              color: white;
-              padding: 15px 32px;
-              text-align: center;
-              text-decoration: none;
-              display: inline-block;
-              font-size: 16px;
-              margin: 4px 2px;
-              cursor: pointer;
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color:#360e94;
             }
+
+        li {
+        float: left;
+        }
+
+        li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        }
+
+        li a:hover {
+        background-color:#ffd004;
+        }
+
+      html {
+        box-sizing: border-box;
+      }
+
+      *, *:before, *:after {
+        box-sizing: inherit;
+      }
+
+      .column {
+        float: left;
+        width: 33.3%;
+        margin-bottom: 16px;
+        padding: 0 8px;
+      }
+
+      .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        margin: 8px;
+      }
+
+      .about-section {
+        padding: 50px;
+        text-align: center;
+        background-color: #360e94;
+        color: white;
+      }
+
+      .container {
+        padding: 0 16px;
+      }
+
+
+
+      .title {
+        color: grey;
+      }
+
+      .button {
+
+        border: none;
+        outline: 0;
+        display: inline-block;
+        padding: 8px;
+        color: white;
+        background-color: #000;
+        text-align: center;
+        cursor: pointer;
+        width: 100%;
+      }
+
+      .button:hover {
+        background-color: #ffb800;
+        color:blue;
+      }
+
+      @media screen and (max-width: 650px) {
+        .column {
+          width: 100%;
+          display: block;
+        }
+      }
+            .button1 {
+            background-color: rgb(54, 14, 148);
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            position: absolute;
+            top: 0;
+            right: 0;
+          }
+
+          .button {
+             background-color: #360e94;
+             border: none;
+             color: white;
+             padding: 15px 32px;
+             text-align: center;
+             text-decoration: none;
+             display: inline-block;
+             font-size: 16px;
+             margin: 4px 2px;
+             cursor: pointer;
+          }
     </style>
 </head>
 <body>
+
+<div class="box">
+        <div>
+            <ul>
+                <li><a href="welcome.php">Početna</a></li>
+                <li><a href="onlineResults.html">Online rezultati</a></li>
+                <li><a href="aboutCandidates.html">O kandidatima</a></li>
+                <li><a href="rules.html">Uputstvo za glasanje</a></li>
+                <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true): ?>
+                  <li><a href="logout.php">Odjavi se</a></li>
+                <?php else: ?>
+                <p class="menu"><a href="login.html">Login</a></p> 
+    <?php endif; ?>
+               
+                
+              </ul>
+        </div>
+    </div>
+
     <h1 class="my-5" style="color:rgb(54, 14, 148);">Zdravo, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Dobrodošli na online glasanje.</h1>
 
 
@@ -125,8 +169,16 @@ html {
 
 <div class="about-section">
   <h1 style="color:yellow;">Uslovi korištenja</h1>
-  <p style="color:yellow;">Nakon odabira opcije "Dalje" dobijate mogućnost biranja kandidata klikom na ponuđeni meni.</p>
-  <p style="color:yellow;">Sa Vaše desne strane ce se nalaziti statistika trenutnog broja glasova prikazanih u postotcima.</p>
+  <p style="color:yellow;">Nakon odabira opcije "Dalje" dobijate mogućnost biranja kandidata sa ponuđene liste.</p>
+
+  <center><button class="button" style="#360e94; font-size: 1.875em;" onclick="myFunction()">Dalje--></button></center>
+        <script>
+            function myFunction() {
+              location.replace("vote.html")
+            }
+        </script>
+
+
 </div>
 
 <h2 style="text-align:center; color:rgb(54, 14, 148);">Podrška</h2>
@@ -140,22 +192,7 @@ html {
         
     </p>
 
-        <center><button class="button" style="#360e94" onclick="myFunction()">Dalje--></button></center>
-        <script>
-            function myFunction() {
-              location.replace("vote.html")
-            }
-        </script>
 
-        <a href="logout.php" class="btn btn-danger ml-3">Odjavi se</a>
-    
-
-<button class="button1" onclick="goBack()">Idi nazad</button>
-
-<script>
-function goBack() {
-  window.history.back();
-}
 
 </script>
         
